d6ca8134ed4325708c6ed781d1cbf1ec9f29d458
Przemysław Skrzyniarz <przemyslaw.hilary.skrzyniarz@gmail.com>
2018-12-09 15:21:17 +0100

    JS w DOM c.d.

---
+++ examples/js-dom/js/1-model-dom.js
@@ -0,0 +1,15 @@
+/* Model DOM */
+
+console.log(document);
+console.dir(document); //pokazuje klucz i wartość
+
+console.log(document.body);
+console.dir(document.body);
+console.log(document.head);
+console.dir(document.head);
+
+/* Obiekt window */
+
+console.log(window);
+
+
