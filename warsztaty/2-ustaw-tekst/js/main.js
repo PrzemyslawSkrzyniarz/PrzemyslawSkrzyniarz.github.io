bf16f7536b9ee2bbd8277947c24bf6ba63ac1322
Przemysław Skrzyniarz <przemyslaw.hilary.skrzyniarz@gmail.com>
2018-12-10 11:20:58 +0100

    poprawka i komentarz

--- warsztaty/2-ustaw-tekst/js/main.js
+++ warsztaty/2-ustaw-tekst/js/main.js
@@ -8,8 +8,15 @@ button.addEventListener('click',
     });
     */
 
+//lub jak poniżej krócej
+
+
 document.querySelector('button').addEventListener('click',
     function () {
         console.log('działa');
         this.innerHTML = 'Zmieniono';
     });
+
+
+
+//Jednak wciąż przy użyciu funkcji anonimowej, której używa się by coś na szybko i małego (parę linijek kodu) przetestować, ale nie w większych projektach (przy większej ilości kodu) czy dla klienta
